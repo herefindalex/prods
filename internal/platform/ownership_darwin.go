f4326caf0e1833f3b68867244e17683001449e02
Alex Chang <herefindalex@gmail.com>
2026-09-16 07:35:46 -0400

    feat: add macOS runtime platform support

---
+++ internal/platform/ownership_darwin.go
@@ -0,0 +1,22 @@
+//go:build darwin
+
+package platform
+
+import (
+	"errors"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+func lockFile(file *os.File) error {
+	err := unix.Flock(int(file.Fd()), unix.LOCK_EX|unix.LOCK_NB)
+	if errors.Is(err, unix.EWOULDBLOCK) || errors.Is(err, unix.EAGAIN) {
+		return ErrAlreadyOwned
+	}
+	return err
+}
+
+func unlockFile(file *os.File) error {
+	return unix.Flock(int(file.Fd()), unix.LOCK_UN)
+}
